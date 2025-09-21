389e98dfce72d395485f60cbe6a03da5b26e0e2f
adnan <adnanaqeel844@gmail.com>
2025-09-21 09:37:22 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,8 +7,8 @@ import openai
 
 # Load environment variables
 load_dotenv()
-TELEGRAM_TOKEN = os.getenv("your telegram-bot-token")
-OPENAI_KEY = os.getenv("openai api key")
+TELEGRAM_TOKEN = os.getenv("8245459837:AAHZN1y2CwRnznhZ5KD8opaiXnilzNy60QY")
+OPENAI_KEY = os.getenv("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")
 
 #Configure OpenAI
 openai.api_key = OPENAI_KEY
@@ -55,3 +55,4 @@ def main():
 if __name__ == "__main__":
 
     main()
+
